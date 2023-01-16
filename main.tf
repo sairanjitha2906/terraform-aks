94b0ee96f3972a48ae094c3e9b5cb645d9a7b0ac
sairanjitha2906 <69158909+sairanjitha2906@users.noreply.github.com>
2023-01-16 10:49:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
+  name                = "example-aks6"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "exampleaks1"
